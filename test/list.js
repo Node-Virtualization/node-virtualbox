0ee3faf8678ca4cac80c0368de285673074b288f
Steffen Roegner <steffen@sroegner.org>
2012-11-13 12:43:44 -0500

    removed argument from list function, listing now includes running status

--- test/list.js
+++ test/list.js
@@ -2,7 +2,7 @@ var util = require('util');
 var virtualbox = require('../lib/virtualbox'),
     args = process.argv.slice(2);
 
-virtualbox.list(args[0] || 'vms', function(list_data, error){
+virtualbox.list(function(list_data, error){
   if(error) throw error;
   if(list_data) {
     console.log(util.inspect(list_data));
