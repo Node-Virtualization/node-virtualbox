aea33ddf5f6c873f0b930d0681c93db5c4284e32
ashley <ashley.yang@trantect.com>
2017-01-17 11:06:58 +0800

    update snapshotRestore test

--- test/snapshot-restore.js
+++ test/snapshot-restore.js
@@ -1,10 +1,8 @@
 "use strict";
 
 var virtualbox = require('../lib/virtualbox'),
-    // vm = process.argv[2],
-    // uuid = process.argv[3];
-    vm = 'hola',
-    uuid = 'test';
+    vm = process.argv[2],
+    uuid = process.argv[3];
 
 virtualbox.snapshotRestore(vm, uuid, (error) => {
   if (error) throw error;
