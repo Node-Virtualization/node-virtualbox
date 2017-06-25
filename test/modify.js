a58ac5286e494171747a03b734e3ee9d1d04643f
Roy Shmeuli <roysh9@gmail.com>
2017-06-25 21:10:23 +0300

    Adding modify function test

---
+++ test/modify.js
@@ -0,0 +1,10 @@
+"use strict";
+
+var virtualbox = require('../lib/virtualbox'),
+    args = process.argv.slice(2);
+
+virtualbox.modify(args[0], args[1], function(error){
+    if(error) {
+        throw error;
+    }
+});
